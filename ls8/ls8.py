a15da67c8fa512cf27062b1b29f327b4574adbe4
Mike Sinnema <msinnema33@gmail.com>
2020-07-24 08:20:56 -0700

    initial commit

---
+++ ls8/ls8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import *
+
+
+cpu = CPU()
+
+if len(sys.argv) > 1:
+    cpu.load(sys.argv[1])
+    cpu.run()
+else:
+    print("please add a program to run")
